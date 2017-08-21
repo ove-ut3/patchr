d99abcf6d082503e6803f70af19a18a17b13a20b
Stéphane Le Hénanff <slehenanff@ups-tlse.fr>
2017-08-21 14:45:02 +0200

    Ajout de la fonction supprimer_doublons_champ

--- R/source_maj.R
+++ R/source_maj.R
@@ -387,3 +387,25 @@ suppression_lignes_fin <- function(table, champs_filtre, ligne_fin = NULL){
   return(table)
 
 }
+
+#' Supprime les doublons d'une table selon un champ
+#'
+#' Supprime les doublons d'une table selon un champ.
+#'
+#' @param table Un data frame.
+#' @param champ Un champ pour lequel les doublons sont supprimés.
+#'
+#' @return un data frame.
+#'
+#' @export
+supprimer_doublons_champ <- function(table, champ) {
+
+  quo_champ <- dplyr::enquo(champ)
+
+  supprimer_doublons_champ <- tidyr::nest(table, !!quo_champ) %>%
+    dplyr::mutate(!!dplyr::quo_name(quo_champ) := purrr::map_chr(data, ~ ifelse(length(.[[1]]) == 1, .[[1]], NA_character_))) %>%
+    dplyr::select(-data)
+
+  return(supprimer_doublons_champ)
+
+}
