a7103d935d9cdb568afd4778285dc44f3357e0bc
Stephane Le Henanff <github@le-henanff.fr>
2018-08-01 13:48:59 +0200

    Anglicisation de renommer_champs -> rename

--- R/rename.R
+++ R/rename.R
@@ -1,60 +1,49 @@
-#' Renomme les champs d'un data frame selon une table de correspondance
+#' Rename data frame column names according to a correspondance table.
 #'
-#' Renomme les champs d'un data frame selon une table de correspondance.
+#' The correspondance table data_remane must at least contains tow columns : column for the old colnames and rename for the new colnames.
 #'
-#' @param table Un data frame.
-#' @param table_rename Une table de correspondance entre anciens et nouveaux noms de champ.
-#' @param source Nom de la source à filtrer dans la table \code{table_rename}.
-#' @param filtre La valeur de filtre.
-#' @param drop \code{TRUE}: les champs non-rensignés dans la table de correspondance sont supprimés; \code{FALSE}: tous les champs sont conservés.
+#' @param data A data frame.
+#' @param data_rename A correspondance table between old and new column names.
+#' @param drop If \code{TRUE} then all the columns not present in data_rename are removed.
 #'
-#' @return Un data frame dont les champs sont renommés.
+#' @return A renamed data frame.
+#'
+#' @examples
+#' dplyr::tibble(var1 = "a", old = "b") %>%
+#'   patchr::rename(dplyr::tibble(column = "old", rename = "new"), drop = FALSE)
 #'
 #' @export
-renommer_champs <- function(table, table_rename, source = NULL, filtre = NULL, drop = TRUE) {
+rename <- function(data, data_rename, drop = TRUE) {
 
-  if (is.null(table_rename)) {
-    return(table)
+  if (any(class(data) == "data.frame") == FALSE) {
+    return(data)
   }
 
-  if (any(class(table) == "data.frame") == FALSE) {
-    return(table)
-  }
-
-  if (!is.null(source)) {
-    table_rename <- dplyr::filter(table_rename, source %in% !!source)
-  }
-
-  if (!is.null(filtre)) {
-    table_rename <- tidyr::separate_rows(table_rename, filtre, sep = ";") %>%
-      dplyr::filter(filtre == !!filtre | is.na(filtre))
-  }
-
-  colnames_maj <- dplyr::data_frame(champ = colnames(table)) %>%
-    dplyr::left_join(dplyr::mutate(table_rename, rename = ifelse(is.na(rename), champ, rename)),
-                     by = "champ")
+  new_colnames <- dplyr::tibble(column = colnames(data)) %>%
+    dplyr::left_join(dplyr::mutate(data_rename, rename = ifelse(is.na(rename), column, rename)),
+                     by = "column")
 
   if (drop == TRUE) {
-    num_champ_supression <- which(is.na(colnames_maj$rename))
-    if (length(num_champ_supression) != 0) {
-      table <- dplyr::select(table, -which(is.na(colnames_maj$rename)))
-      colnames_maj <- dplyr::filter(colnames_maj, !is.na(rename))
+    column_drop <- which(is.na(new_colnames$rename))
+    if (length(column_drop) != 0) {
+      data <- dplyr::select(data, -which(is.na(new_colnames$rename)))
+      new_colnames <- dplyr::filter(new_colnames, !is.na(rename))
     }
   } else if (drop == FALSE){
-    colnames_maj <- dplyr::mutate(colnames_maj, rename = ifelse(!is.na(rename), rename, caractr::str_camel_to_snake_case(champ)))
+    new_colnames <- dplyr::mutate(new_colnames, rename = ifelse(!is.na(rename), rename, caractr::str_camel_to_snake_case(column)))
   }
 
-  colnames_maj <- colnames_maj[["rename"]]
+  new_colnames <- new_colnames[["rename"]]
 
-  if (length(unique(colnames_maj)) != length(colnames_maj)) {
-    doublons <- which(table(colnames_maj) >= 2) %>% names()
-    message("Doublon dans la table_rename sur le champ \"rename\" : ", paste0(doublons, collapse = ", "))
-    colnames_maj <- make.unique(colnames_maj)
+  if (length(unique(new_colnames)) != length(new_colnames)) {
+    duplicate <- which(data(new_colnames) >= 2) %>% names()
+    message("Duplicate in data_rename on column \"rename\" : ", paste0(duplicate, collapse = ", "))
+    new_colnames <- make.unique(new_colnames)
   }
 
-  colnames(table) <- colnames_maj
+  colnames(data) <- new_colnames
 
-  if (ncol(table) == 0) return(NULL)
-  else return(table)
+  if (ncol(data) == 0) return(NULL)
+  else return(data)
 
 }
