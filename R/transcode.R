855c20329fa9174f26d36d1cc602fc2f0fcc5916
Stéphane Le Hénanff <github@le-henanff.fr>
2019-09-02 13:45:17 +0200

    export as_date function

--- R/transcode.R
+++ R/transcode.R
@@ -61,3 +61,93 @@ transcode <- function(data, data_transcode) {
   }
 
 }
+#' Convert a character date to POSIXct
+#'
+#' @param x Character date
+#' @param origin Origin date
+#'
+#' @return A POSIXct date
+#'
+#'@export
+as_date <- function(x, origin = "1899-12-30") {
+
+  as_date <- as.Date(rep(NA, length(x)))
+
+  x <- stringr::str_remove(x, "\\d{2}:\\d{2}:\\d{2}$") %>%
+    stringr::str_remove("\\d{2}:\\d{2}$") %>%
+    trimws()
+
+  if (any(class(x) %in% c("character", "factor")) == TRUE) {
+
+    position <- stringr::str_detect(x, "^\\d{5}(\\.0+)?$") %>% which()
+    if (length(position) >= 1) {
+      as_date[position] <- as.Date.numeric(as.integer(x[position]), origin)
+    }
+
+    position <- stringr::str_detect(x, "^\\d{1,2}/\\d{1,2}/\\d{4}$") %>% which()
+    if (length(position) >= 1) {
+      as_date[position] <- as.Date(x[position], "%d/%m/%Y")
+    }
+
+    position <- stringr::str_detect(x, "^\\d{1,2}/\\d{4}$") %>% which()
+    if (length(position) >= 1) {
+      as_date[position] <- as.Date(paste0("01/", x[position]), "%d/%m/%Y")
+    }
+
+    position <- stringr::str_detect(x, "^\\d{1,2}-\\d{1,2}-\\d{4}$") %>% which()
+    if (length(position) >= 1) {
+      as_date[position] <- as.Date(x[position], "%d-%m-%Y")
+    }
+
+    position <- stringr::str_detect(x, "^\\d{4}-\\d{1,2}-\\d{1,2}$") %>% which()
+    if (length(position) >= 1) {
+      as_date[position] <- as.Date(x[position])
+    }
+
+    position <- stringr::str_detect(x, "^\\d{4}/\\d{1,2}/\\d{1,2}$") %>% which()
+    if (length(position) >= 1) {
+      as_date[position] <- as.Date(x[position])
+    }
+
+    #libell\u00E9 de mois (excel)
+    if (any(stringr::str_detect(x, "^[[:alpha:]]{3,4}-"), na.rm = TRUE)) {
+
+      date_char <- x %>%
+        stringr::str_subset("^[[:alpha:]]{3,4}-") %>%
+        stringr::str_match("^([[:alpha:]]{3,4})-(\\d{2})$") %>%
+        as.data.frame() %>%
+        dplyr::as_tibble() %>%
+        dplyr::mutate(
+          date_char = paste0(
+            dplyr::recode(V2, "janv" = "01", "f\u00E9vr" = "02", "mars" = "03", "avr" = "04", "mai" = "05", "juin" = "06", "juil" = "07", "ao\u00FBt" = "08", "sept" = "09", "oct" = "10", "nov" = "11", "d\u00E9c" = "12"),
+            "/",
+            ifelse(as.numeric(V3) >= 76, paste0("19", V3), paste0("20", V3))
+          ) %>%
+          paste0("01/", .)
+        )
+
+      as_date[stringr::str_which(x, "^[[:alpha:]]{3,4}-")] <- date_char$date_char
+
+    }
+
+    return(as_date)
+  }
+
+  if (any(class(x) %in% c("numeric", "integer")) == TRUE) {
+
+    position <- stringr::str_detect(as.character(x), "^\\d{5}$") %>% which()
+    if (length(position) >= 1) {
+      as_date[position] <- as.Date.numeric(x[position], origin)
+    }
+
+    return(as_date)
+  }
+
+  if (any(class(x) == "POSIXct") == TRUE) {
+    return(as.Date.POSIXct(x))
+  }
+
+  if (any(class(x) == "logical") == TRUE) {
+    return(as_date)
+  }
+}
